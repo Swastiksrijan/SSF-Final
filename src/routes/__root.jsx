dc67b47c27df53e4a23589c5dd263ea179cec0fb
Swastik Srijan Foundation <swastiksrijanfoundation@gmail.com>
2026-08-02 00:04:11 +0530

    chore(ui): move EliteDonationCard out of global root (will place in Home page)

--- src/routes/__root.jsx
+++ src/routes/__root.jsx
@@ -1,6 +1,5 @@
 import { createRootRoute, Outlet } from "@tanstack/react-router";
 import Header from "../components/Header";
-import EliteDonationCard from "../components/EliteDonationCard";
 import FooterSection from "../pages/FooterSection";
 import ScrollToHash from "../components/ScrollToHash";
 import ScrollToTopButton from "../components/ScrollToTopButton";
@@ -14,7 +13,6 @@ export const Route = createRootRoute({
       <div className="min-h-screen w-full flex flex-col font-inria overflow-x-hidden">
         <SeoManager />
         <Header />
-        <EliteDonationCard />
 
         <ScrollToHash />
         <ScrollToTopButton />
