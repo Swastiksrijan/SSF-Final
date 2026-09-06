9fe03084107887231606a56a3c558b8582c4eea0
Swastik Srijan Foundation <swastiksrijanfoundation@gmail.com>
2026-09-06 10:40:46 +0530

    Remove incorrectly named duplicate verification route

--- src/routes/VerifyDocument.jsx
+++
@@ -1,6 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-import VerifyDocument from "../pages/VerifyDocument";
-
-export const Route = createFileRoute("/verify/$code")({
-  component: VerifyDocument,
-});
