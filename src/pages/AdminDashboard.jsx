36727913f984d41dc0e2a654296df7da5a0af1e4
Swastik Srijan Foundation <swastiksrijanfoundation@gmail.com>
2026-09-06 08:31:54 +0530

    feat: combine admin portal with unified applications

---
+++ src/pages/AdminDashboard.jsx
@@ -0,0 +1,9 @@
+import AdminPortalV2 from './AdminPortalV2';
+import AdminApplicationDashboard from '../components/AdminApplicationDashboard';
+
+export default function AdminDashboard() {
+    return <>
+        <AdminPortalV2 />
+        <AdminApplicationDashboard />
+    </>;
+}
