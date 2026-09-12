f247f1aaf430fd67c28058606fb171dc1da3bcd7
Swastik Srijan Foundation <swastiksrijanfoundation@gmail.com>
2026-09-12 21:05:02 +0530

    fix: remove application status block from user profile page

--- src/routes/UserPortal.jsx
+++ src/routes/UserPortal.jsx
@@ -1,13 +1,11 @@
 import { createFileRoute } from "@tanstack/react-router";
 import UserPortal from "../pages/UserPortal";
 import JoinSSFHub from "../components/JoinSSFHub";
-import UserApplicationStatus from "../components/UserApplicationStatus";
 
 function UserPortalWithJoin() {
   return (
     <>
       <JoinSSFHub />
-      <UserApplicationStatus />
       <UserPortal />
     </>
   );
