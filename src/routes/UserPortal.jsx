9c730ee848d1585c4c7a49b86faf45a58b2b9ca3
Swastik Srijan Foundation <swastiksrijanfoundation@gmail.com>
2026-09-12 19:37:52 +0530

    feat: show SSF participation hub in user portal

--- src/routes/UserPortal.jsx
+++ src/routes/UserPortal.jsx
@@ -1,6 +1,16 @@
 import { createFileRoute } from "@tanstack/react-router";
 import UserPortal from "../pages/UserPortal";
+import JoinSSFHub from "../components/JoinSSFHub";
+
+function UserPortalWithJoin() {
+  return (
+    <>
+      <UserPortal />
+      <JoinSSFHub />
+    </>
+  );
+}
 
 export const Route = createFileRoute("/UserPortal")({
-  component: UserPortal,
+  component: UserPortalWithJoin,
 });
