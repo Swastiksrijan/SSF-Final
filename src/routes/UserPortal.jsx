85eb57e7ec0ca87a8e1115eb8a1c380acb61a9bd
Swastik Srijan Foundation <swastiksrijanfoundation@gmail.com>
2026-09-12 21:11:03 +0530

    refactor: simplify user portal route

--- src/routes/UserPortal.jsx
+++ src/routes/UserPortal.jsx
@@ -1,16 +1,6 @@
 import { createFileRoute } from "@tanstack/react-router";
 import UserPortal from "../pages/UserPortal";
-import JoinSSFHub from "../components/JoinSSFHub";
-
-function UserPortalWithJoin() {
-  return (
-    <>
-      <JoinSSFHub />
-      <UserPortal />
-    </>
-  );
-}
 
 export const Route = createFileRoute("/UserPortal")({
-  component: UserPortalWithJoin,
+  component: UserPortal,
 });
