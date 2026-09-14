29e203d90dc664e1a137215a33933ba4354ecad9
Swastik Srijan Foundation <swastiksrijanfoundation@gmail.com>
2026-09-14 14:31:56 +0530

    Add 2013-2026 journey chart to Impact page

--- src/routes/Impact.jsx
+++ src/routes/Impact.jsx
@@ -1,6 +1,16 @@
 import { createFileRoute } from '@tanstack/react-router'
 import ImpactPage from '../pages/ImpactPage'
+import ServiceJourneyChart from '../components/ServiceJourneyChart'
+
+function ImpactWithJourney() {
+  return (
+    <>
+      <ImpactPage />
+      <ServiceJourneyChart />
+    </>
+  )
+}
 
 export const Route = createFileRoute('/Impact')({
-    component: ImpactPage,
+  component: ImpactWithJourney,
 })
