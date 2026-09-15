a49a9f38adc227fb251e27904496547bb2c9959c
Swastik Srijan Foundation <swastiksrijanfoundation@gmail.com>
2026-09-16 02:11:48 +0530

    Add Blog knowledge hub navigation

--- src/routes/Blog.jsx
+++ src/routes/Blog.jsx
@@ -4,11 +4,13 @@ import Blog from "../pages/Blog";
 import BlogShareEnhancer from "../components/BlogShareEnhancer";
 import SingleKnowledgeStory from "../components/SingleKnowledgeStory";
 import BlogKnowledgeStories from "../components/BlogKnowledgeStories";
+import BlogHubHeader from "../components/BlogHubHeader";
 
 function BlogRoutePage() {
   return (
     <BlogShareEnhancer>
       <div className="pt-28 bg-black min-h-screen text-white px-4">
+        <BlogHubHeader />
         <SingleKnowledgeStory />
         <BlogKnowledgeStories>
           <Blog />
