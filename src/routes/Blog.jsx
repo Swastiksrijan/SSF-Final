7fc8ed5247a9af6d34a3d8d7eeae305d5c837387
Swastik Srijan Foundation <swastiksrijanfoundation@gmail.com>
2026-09-16 02:07:07 +0530

    Show five new knowledge stories on SSF Blog

--- src/routes/Blog.jsx
+++ src/routes/Blog.jsx
@@ -3,13 +3,16 @@ import { createFileRoute } from "@tanstack/react-router";
 import Blog from "../pages/Blog";
 import BlogShareEnhancer from "../components/BlogShareEnhancer";
 import SingleKnowledgeStory from "../components/SingleKnowledgeStory";
+import BlogKnowledgeStories from "../components/BlogKnowledgeStories";
 
 function BlogRoutePage() {
   return (
     <BlogShareEnhancer>
       <div className="pt-28 bg-black min-h-screen text-white px-4">
         <SingleKnowledgeStory />
-        <Blog />
+        <BlogKnowledgeStories>
+          <Blog />
+        </BlogKnowledgeStories>
       </div>
     </BlogShareEnhancer>
   );
