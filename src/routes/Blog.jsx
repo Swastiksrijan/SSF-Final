df5541e49561f740df3894720032cc2e0a13af04
Swastik Srijan Foundation <swastiksrijanfoundation@gmail.com>
2026-09-16 01:42:09 +0530

    Add share button to every Blog & Stories card

--- src/routes/Blog.jsx
+++ src/routes/Blog.jsx
@@ -1,7 +1,16 @@
 // src/routes/Blog.jsx
 import { createFileRoute } from "@tanstack/react-router";
 import Blog from "../pages/Blog";
+import BlogShareEnhancer from "../components/BlogShareEnhancer";
+
+function BlogRoutePage() {
+  return (
+    <BlogShareEnhancer>
+      <Blog />
+    </BlogShareEnhancer>
+  );
+}
 
 export const Route = createFileRoute("/Blog")({
-  component: Blog,
+  component: BlogRoutePage,
 });
