1ed87bd858d1e9d53a826e83ef7a8acf34b8108d
Swastik Srijan Foundation <swastiksrijanfoundation@gmail.com>
2026-09-25 22:02:57 +0530

    Keep Our Initiatives route clean after evidence move

--- src/routes/OurInitiatives.jsx
+++ src/routes/OurInitiatives.jsx
@@ -1,11 +1,6 @@
 import { createFileRoute } from '@tanstack/react-router'
 import OurInitiativesPage from '../pages/OurInitiativesPage'
-import WorkEvidenceBanner from '../components/WorkEvidenceBanner'
-
-function OurInitiativesWithEvidence() {
-  return <><WorkEvidenceBanner page="initiatives" /><OurInitiativesPage /></>
-}
 
 export const Route = createFileRoute('/OurInitiatives')({
-  component: OurInitiativesWithEvidence,
+  component: OurInitiativesPage,
 })
