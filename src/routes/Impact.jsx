a7aa6686fc6386ac526eaf084d5c10a843390ab6
Swastik Srijan Foundation <swastiksrijanfoundation@gmail.com>
2026-09-25 21:57:00 +0530

    Place Impact evidence content below page hero

--- src/routes/Impact.jsx
+++ src/routes/Impact.jsx
@@ -1,12 +1,10 @@
 import { createFileRoute } from '@tanstack/react-router'
 import ImpactPage from '../pages/ImpactPage'
 import ImpactTimeline from '../components/ImpactTimeline'
-import WorkEvidenceBanner from '../components/WorkEvidenceBanner'
 
 function ImpactWithJourney() {
   return (
     <>
-      <WorkEvidenceBanner page="impact" />
       <ImpactPage />
       <ImpactTimeline />
     </>
