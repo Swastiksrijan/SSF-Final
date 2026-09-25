002351f738e0192bb354f391496f9481380b144d
Swastik Srijan Foundation <swastiksrijanfoundation@gmail.com>
2026-09-25 22:43:47 +0530

    Place strategic direction inside page after header

--- src/routes/UpcomingProjects.jsx
+++ src/routes/UpcomingProjects.jsx
@@ -1,9 +1,7 @@
 import { createFileRoute } from '@tanstack/react-router'
 import UpcomingProjects from '../pages/UpcomingProjects'
-import UpcomingEvidenceBanner from '../components/UpcomingEvidenceBanner'
-
 function UpcomingProjectsWithEvidence() {
-  return <><UpcomingProjects /><UpcomingEvidenceBanner /></>
+  return <UpcomingProjects />
 }
 
 export const Route = createFileRoute('/UpcomingProjects')({
