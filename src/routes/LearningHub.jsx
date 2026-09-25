de85d33ee5c1caf4172d6f7846da303320833a73
Swastik Srijan Foundation <swastiksrijanfoundation@gmail.com>
2026-09-25 22:11:45 +0530

    Move Learning Hub evidence section below header

--- src/routes/LearningHub.jsx
+++ src/routes/LearningHub.jsx
@@ -1,9 +1,8 @@
 import { createFileRoute } from "@tanstack/react-router";
 import LearningHub from "../pages/LearningHub";
-import WorkEvidenceBanner from "../components/WorkEvidenceBanner";
 
 function LearningHubWithEvidence() {
-  return <><WorkEvidenceBanner page="learning" /><LearningHub /></>
+  return <LearningHub />
 }
 
 export const Route = createFileRoute("/LearningHub")({
